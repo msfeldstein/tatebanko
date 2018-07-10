6fe09604230c7afb672158df0b6032ce3b5e19cd
Glitch (tate-banking) <none>
2018-07-10 02:39:29 +0000

    🐵🏇 Checkpoint ./src/index.js:286337/1043

--- src/index.js
+++ src/index.js
@@ -13,7 +13,16 @@ const {
   document.body,
 )
 
-for (var i = 1; i < 6; i++) {
- const url = `https://cdn.glitch.com/ae47162c-cd2d-4028-9d28-856d2312c256%2F${i}.png?1531189617683` 
- console.log(url)
+function attachImage(index, image) {
+  
 }
+
+async function loadInitialImages() {
+  for (var i = 1; i < 2; i++) {
+    const url = `https://cdn.glitch.com/ae47162c-cd2d-4028-9d28-856d2312c256%2F${i}.png?1531189617683` 
+    let image = new Image()
+    image.onload = attachImage(i,
+  }  
+}
+
+loadInitialImages()
