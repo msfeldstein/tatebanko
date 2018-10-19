cb9c375cce265c1eb75010b637e8eb06d2ec5fbf
Mike Feldstein <feldstein@fb.com>
2018-10-18 21:03:13 -0700

    Update to real specs

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -6,6 +6,11 @@ const DEFAULT_V_FOV = 0.666488587;
 const DEFAULT_H_FOV = 0.508015513;
 const ASPECT_RATIO = .75//Math.tan(DEFAULT_H_FOV / 2) / Math.tan(DEFAULT_V_FOV / 2);
 const FOV = 35.32295939408245
+const ROTATION_FACTOR = 0.06;
+const CAMERA_RADIUS = 1;
+const GYRO_SPEED = 0.005;
+const MAX_ZOOM = 0.1;
+
 const renderer = new THREE.WebGLRenderer({
   antialias: true,
   autoClear: false,
@@ -19,7 +24,6 @@ const layerCache = {}
 const scene = new THREE.Scene()
 window.scene = scene
 const camera = new THREE.PerspectiveCamera(FOV, ASPECT_RATIO, 0.1, 3000)
-console.log(FOV, 'FOV')
 let composerCamera = new THREE.PerspectiveCamera(FOV, renderer.getSize().width / renderer.getSize().height / 2, 0.1, 2000)
 composerCamera.position.set(2, 2, 2)
 composerCamera.lookAt(new THREE.Vector3(0, 0, -3))
@@ -39,7 +43,26 @@ group.position.z = 1
 scene.add(pivot)
 pivot.position.z = -1
 
+let photo3dXTarget = 0
+let photo3dYTarget = 0
+
+renderer.domElement.addEventListener('mousemove', e => {
+  const w = renderer.getSize().width / 2
+  const h = renderer.getSize().height
+  const paddedW = Math.min(w - 40, (h - 80) * 3 / 4)
+  const paddedH = paddedW * 4 / 3
+  const padLeft = (w - paddedW) / 2
+  const padTop = (h - paddedH) / 4
+  photo3dXTarget = ((e.offsetX - padLeft) / paddedW)
+  photo3dYTarget = ((e.offsetY - padTop) / paddedH)
+  photo3dXTarget = 1 - Math.min(1, Math.max(photo3dXTarget, 0))
+  photo3dYTarget = Math.min(1, Math.max(photo3dYTarget, 0))
+  photo3dXTarget = photo3dXTarget * 0.1 - 0.05
+  photo3dYTarget = photo3dYTarget * 0.1 - 0.05
+})
+
 const origin = new THREE.Vector3()
+const cameraTarget = new THREE.Vector3()
 function render(t) {
   requestAnimationFrame(render)
   if (!camera) return
@@ -61,13 +84,46 @@ function render(t) {
   renderer.clear()
   renderer.setScissorTest(false)
   renderer.setViewport(padLeft, padTop, paddedW, paddedH)
-  // pivot.rotation.x = Math.sin(t / 500) * .02
-  // pivot.rotation.y = Math.cos(t / 500) * .02
-  camera.position.set(
-    .01 * Math.cos(t / 1000),
-    .01 * Math.sin(t / 1000),
-    0
-  )
+
+  const percent = 0
+  const pitch = photo3dYTarget
+  const yaw = photo3dXTarget
+  const scrollPitch = 0
+  const targetDistributeH = 0.6
+  const targetDistributeV = 0.7
+  const targetMaxH = -0.03219230681522755
+  const targetMaxV = -0.03876922981457388
+
+  const realPhoto3dY = Math.sin(photo3dYTarget * Math.PI * 0.5); // [-1, 1]
+  const realPhoto3dX = Math.sin(photo3dXTarget * Math.PI * 0.5);
+  const targetV = -realPhoto3dY * targetMaxV; // [_targetMaxV, _targetMaxV]
+  const targetH = -realPhoto3dX * targetMaxH;
+  const zoom =
+      Math.pow(percent + 0.16, 3) /
+        (Math.pow(percent + 0.16, 3) + Math.pow(1 - (percent + 0.16), 3)) -
+      0.9;
+    const newPitch = -(pitch + scrollPitch); // [-2*vfov, 2*vfov]
+    const newYaw = -yaw;
+    const r = CAMERA_RADIUS - zoom * MAX_ZOOM;
+
+    // target is a combination of scroll and user interaction
+    cameraTarget.set(
+      -((1 - targetDistributeH) * targetH),
+      -(1 - targetDistributeV) * targetV,
+      -CAMERA_RADIUS,
+    ); // [scrollTargetV, targetV]
+    camera.position.set(
+      0 + r * Math.cos(newPitch) * Math.sin(newYaw),
+      0 + r * Math.sin(newPitch),
+      -CAMERA_RADIUS + r * Math.cos(newPitch) * Math.cos(newYaw),
+    );
+    camera.lookAt(cameraTarget);
+
+
+
+
+
+
   camera.lookAt(pivot.position)
   cameraHelper.visible = false
   renderer.render(scene, camera)
@@ -102,7 +158,7 @@ function getOrCreateFromImageData(imageData) {
     plane.name = imageData.src
     const planePivot = new THREE.Object3D()
     planePivot.add(plane)
-    plane.position.z = -.858
+    plane.position.z = -.73
     
     if (DEBUG) {
       const debugBox = new THREE.Mesh(
--- src/index.js
+++ src/index.js
@@ -15,11 +15,20 @@ const appActions = app(
   document.body,
 )
 
+// australia
+// const initialImages = [
+// 	'https://cdn.glitch.com/ae47162c-cd2d-4028-9d28-856d2312c256%2Faustralia_0.png',
+// 	'https://cdn.glitch.com/ae47162c-cd2d-4028-9d28-856d2312c256%2Faustralia_1.png',
+// 	'https://cdn.glitch.com/ae47162c-cd2d-4028-9d28-856d2312c256%2Faustralia_2.png',
+// 	'https://cdn.glitch.com/ae47162c-cd2d-4028-9d28-856d2312c256%2Faustralia_3.jpg',
+// ]
+
+// vox machina
 const initialImages = [
-	'https://cdn.glitch.com/ae47162c-cd2d-4028-9d28-856d2312c256%2Faustralia_0.png',
-	'https://cdn.glitch.com/ae47162c-cd2d-4028-9d28-856d2312c256%2Faustralia_1.png',
-	'https://cdn.glitch.com/ae47162c-cd2d-4028-9d28-856d2312c256%2Faustralia_2.png',
-	'https://cdn.glitch.com/ae47162c-cd2d-4028-9d28-856d2312c256%2Faustralia_3.jpg',
+  'https://cdn.glitch.com/ae47162c-cd2d-4028-9d28-856d2312c256%2Ftitle.png?1539921589026',
+  'https://cdn.glitch.com/ae47162c-cd2d-4028-9d28-856d2312c256%2Frobots.png?1539921591261',
+  'https://cdn.glitch.com/ae47162c-cd2d-4028-9d28-856d2312c256%2Fmountains.png?1539921586269',
+  'https://cdn.glitch.com/ae47162c-cd2d-4028-9d28-856d2312c256%2FBG.png?1539921592656'
 ]
 
 // const initialImages = [
